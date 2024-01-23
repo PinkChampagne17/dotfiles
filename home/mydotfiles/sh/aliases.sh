420eba25310668087035cab669da33c6dfdf4ea9
Pink Champagne <45930107+PinkChampagne17@users.noreply.github.com>
2024-01-23 15:31:56 +0800

    refactor: remove template syntax

--- home/mydotfiles/sh/aliases.sh
+++ home/mydotfiles/sh/aliases.sh
@@ -1,7 +1,11 @@
 alias cwd="code ." # code work dir
 alias ll="ls -AFhl"
-alias md="mkdir"
+alias md=mkdir
 alias ni=touch
 alias pn=pnpm
 alias pips="pn i && pn start"
 alias pipd="pn i && pn dev"
+
+if [[ "$OSTYPE" == "msys" ]]; then
+    alias open=explorer
+fi
