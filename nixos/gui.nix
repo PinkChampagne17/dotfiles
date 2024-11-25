6672b38e6ec5380e0c17df73dee3bb759a188468
Pink Champagne <45930107+PinkChampagne17@users.noreply.github.com>
2024-11-25 10:37:07 +0800

    update nixos

--- nixos/gui.nix
+++ nixos/gui.nix
@@ -1,21 +1,42 @@
 { pkgs, ... }:
 
 {
-  imports = [ ./gnome.nix ];
+  imports = [
+    ./gnome.nix
+  ];
 
   i18n.inputMethod = {
-    enabled = "fcitx5";
+    enable = true;
+    type = "fcitx5";
     fcitx5.addons = with pkgs; [ fcitx5-chinese-addons ];
     fcitx5.waylandFrontend = true;
   };
 
-  fonts.packages = with pkgs; [
-    cascadia-code
-    noto-fonts-emoji
-    source-han-sans
-    source-han-mono
-    source-han-serif
-  ];
+  fonts = {
+    fontconfig = {
+      defaultFonts = {
+        serif = [
+          "Source Han Serif SC"
+          "Source Han Serif"
+        ];
+        sansSerif = [
+          "Source Han Sans SC"
+          "Source Han Sans"
+        ];
+        monospace = [
+          "Source Han Mono SC"
+          "Source Han Mono"
+        ];
+      };
+    };
+    packages = with pkgs; [
+      cascadia-code
+      noto-fonts-color-emoji
+      source-han-sans
+      source-han-mono
+      source-han-serif
+    ];
+  };
 
   # Fcitx not work
   # environment.sessionVariables.NIXOS_OZONE_WL = "1";
@@ -24,6 +45,8 @@
     baobab
     firefox
     kdePackages.partitionmanager
+    nixd
+    nvtopPackages.full
     papirus-icon-theme
     ptyxis
     resources
