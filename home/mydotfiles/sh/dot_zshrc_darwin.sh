8798c17fd2dce783ad8df976dd76dbd30a5c7f9c
Pink Champagne <45930107+PinkChampagne17@users.noreply.github.com>
2025-02-10 09:56:35 +0800

    add ghostty config

--- home/mydotfiles/sh/dot_zshrc_darwin.sh
+++ home/mydotfiles/sh/dot_zshrc_darwin.sh
@@ -1,7 +1,7 @@
 # pnpm
-# export PNPM_HOME="$HOME/Library/pnpm"
-# case ":$PATH:" in
-#   *":$PNPM_HOME:"*) ;;
-#   *) export PATH="$PNPM_HOME:$PATH" ;;
-# esac
+export PNPM_HOME="$HOME/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
 # pnpm end
